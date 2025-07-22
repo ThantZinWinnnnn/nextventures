e573d07baa0c92c18708f9ded7e5a4b729170bd6
thantzinwinn <thant.zin.winnnn@gmail.com>
2025-07-22 22:50:55 +0630

    delete dev test file

--- e2e/search-text.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("Search Playwright Text", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-
-  expect(page.getByText(/Welcome to Playwright Testing with Nextjs/));
-});
